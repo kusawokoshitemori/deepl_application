ea7af52a8af8127e6cbf8b7df3a001e1cb43d765
草をこして森 <kyou0317kyou0317@gmail.com>
2024-10-05 00:12:16 +0900

    add頁追加してみただけ

---
+++ deepl-application/src/pages/add/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AddPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Add Page</h1>
+      {/* ここに追加したい内容を書くことができます */}
+    </div>
+  );
+};
+
+export default AddPage;
--- deepl-application/src/pages/index.tsx
+++ deepl-application/src/pages/index.tsx
@@ -2,6 +2,7 @@
 
 import Head from "next/head";
 import TranslateForm from "../components/TranslateForm";
+import Link from "next/link";
 
 const Home: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const Home: React.FC = () => {
       </Head>
       <h1 className="text-4xl font-bold mb-8">DeepL Translator</h1>
       <TranslateForm />
+      <Link href="/add">Add Pageに行く</Link>
     </div>
   );
 };
