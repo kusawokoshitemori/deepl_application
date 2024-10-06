6bbe153f9a48a608464055cecd90f4706d21052e
草をこして森 <kyou0317kyou0317@gmail.com>
2024-10-06 21:30:50 +0900

    ホーム画面ほぼ完成

--- deepl-application/src/components/Bigbutton.tsx
+++ deepl-application/src/components/Bigbutton.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 interface BigButtonProps {
   Buttonname: string;
+  className?: string;
 }
 
-const Bigbutton: React.FC<BigButtonProps> = ({ Buttonname }) => {
+const Bigbutton: React.FC<BigButtonProps> = ({ Buttonname, className }) => {
   return (
-    <button className="bg-blue-500 text-white font-bold rounded hover:bg-blue-600 transition duration-300 ease-in-out w-1/2 text-4xl ">
+    <button
+      style={{ fontSize: "48px" }}
+      className={`lg:w-3/4 h-32 bg-blue-500 text-white font-bold rounded  ${className}`}
+    >
       {Buttonname}
     </button>
   );
--- deepl-application/src/pages/index.tsx
+++ deepl-application/src/pages/index.tsx
@@ -1,13 +1,23 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 import Bigbutton from "../components/Bigbutton";
 
 const Home: React.FC = () => {
   return (
-    <>
+    <div className="flex items-center justify-center flex-col bg-slate-200 h-screen">
       <Link href="/sample">サンプルページへ飛ぶ</Link>
-      <Bigbutton Buttonname="クリックしてください" />
-    </>
+      {/* ここに画像を入れたい */}
+      <Image
+        src="/images/proverb_title.png" // publicフォルダからのパス
+        alt="説明文" // 画像の代替テキスト
+        width={650} // 画像の幅
+        height={300} // 画像の高さ
+      />
+      <Bigbutton Buttonname="ゲーム開始" className="text-lime-200 m-4" />
+      <Bigbutton Buttonname="ことわざ追加" className="m-4" />
+      <Bigbutton Buttonname="設定" className="m-4" />
+    </div>
   );
 };
 
